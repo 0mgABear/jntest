9795ca8bf6a02f1d784c93fc8d46f29ece27efc7
0mgABear <yeoyi94@gmail.com>
2025-02-08 16:08:30 +0800

    update both files

--- table_creation.sql
+++ table_creation.sql
@@ -1,2 +1,3 @@
 create or replace table t_test 
 id varchar(10)
+[name] varchar(30)
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 def main():
   print('hellol world')
 
+def test():
+  print('this is a dummy update')
+
 if __name__ == "__main__":
   main()
